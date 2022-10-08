5d1d594beaf61e0d775a1aebfb4475cbc12fc27f
Renan Santos <dosantosrenansilva@gmail.com>
2022-10-08 18:24:55 -0300

    You will need create an document.txt in temp to run this program properly.

---
+++ LogRecord/Entities/RegistroDeLog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogRecord.Entities
+{
+    internal class RegistroDeLog
+    {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is RegistroDeLog))
+                {
+                return false;
+            }
+
+            RegistroDeLog other = obj as RegistroDeLog;
+
+            return Username.Equals(other.Username);
+        }
+    }
+}
---
+++ LogRecord/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using LogRecord.Entities;
+
+namespace LogRecord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<RegistroDeLog> set = new HashSet<RegistroDeLog>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new RegistroDeLog { Username = name, Instant = instant });
+                        
+                    }
+                    Console.WriteLine("Total users: " + set.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
